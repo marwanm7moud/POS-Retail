6dacf800ba18d3de654535b15262db76870045d9
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-05-28 17:12:41 +0300

    add home ui

--- composeApp/src/commonMain/kotlin/org/abapps/app/resource/strings/IStringResources.kt
+++ composeApp/src/commonMain/kotlin/org/abapps/app/resource/strings/IStringResources.kt
@@ -5,4 +5,23 @@ interface IStringResources {
     val transfer: String
     val settings: String
     val exit: String
+    val retry: String
+    val warning: String
+    val doYouWantToCloseApp: String
+    val error: String
+    val enterYourInfo: String
+    val userName: String
+    val password: String
+    val ok: String
+    val no: String
+    val yes: String
+    val cancel: String
+    val tryAgain: String
+    val somethingWrongHappened: String
+    val noInternetMessage: String
+    val noInternetDescription: String
+    val nullDataMessage: String
+    val nullDataDescription: String
+    val userNotFound: String
+    val enterYourPasscode: String
 }
