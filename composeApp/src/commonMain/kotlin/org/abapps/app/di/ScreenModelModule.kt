d34ef6cd30c7a23973688b048dbb24e909442c75
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-05-29 01:32:11 +0300

    finish settings screen

--- composeApp/src/commonMain/kotlin/org/abapps/app/di/ScreenModelModule.kt
+++ composeApp/src/commonMain/kotlin/org/abapps/app/di/ScreenModelModule.kt
@@ -4,6 +4,7 @@ import org.abapps.app.presentation.app.AppScreenModel
 import org.abapps.app.presentation.screens.allinvoices.AllInvoicesScreenModel
 import org.abapps.app.presentation.screens.home.HomeScreenModel
 import org.abapps.app.presentation.screens.posInvoiceScreen.InvoiceScreenModel
+import org.abapps.app.presentation.screens.settings.SettingsScreenModel
 import org.abapps.app.presentation.screens.transferInvoices.TransferInvoicesScreenModel
 import org.abapps.app.presentation.screens.transferNewInvoice.TransferNewInvoiceScreenModel
 import org.koin.core.module.dsl.factoryOf
@@ -16,4 +17,5 @@ val screenModelModule = module {
     factoryOf(::TransferInvoicesScreenModel)
     factoryOf(::TransferNewInvoiceScreenModel)
     factoryOf(::HomeScreenModel)
+    factoryOf(::SettingsScreenModel)
 }
