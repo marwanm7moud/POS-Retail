4ed703eb903813fe3d277840734b54a20680c6c9
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-05-29 16:58:56 +0300

    add CalculationInvoiceUseCase

--- composeApp/src/commonMain/kotlin/org/abapps/app/domain/usecase/CalculationInvoiceUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/abapps/app/domain/usecase/CalculationInvoiceUseCase.kt
@@ -1,12 +1,29 @@
 package org.abapps.app.domain.usecase
 
+import org.abapps.app.data.util.RetailSetup
 import org.abapps.app.presentation.screens.posInvoiceScreen.NewInvoiceItemUiState
 
 class CalculationInvoiceUseCase {
 
     fun calculateItemsPrice(items: List<NewInvoiceItemUiState>): List<NewInvoiceItemUiState> {
         return items.map { item ->
-            item.copy(extPrice = item.price * item.qty.toFloat())
+            if (!RetailSetup.VAT && ((!RetailSetup.TAX_EFFECT && !RetailSetup.TAX_EFFECT_WITH_ITEM))
+                || (RetailSetup.TAX_EFFECT && RetailSetup.TAX_EFFECT_WITH_ITEM)
+            ) {
+                val priceWOT = item.orgPrice
+                val taxAmount = item.taxPerc.div(100f) * priceWOT
+                val price = priceWOT + taxAmount
+                val extPrice = price * item.qty.toFloat()
+                item.copy(
+                    priceWOT = priceWOT,
+                    taxAmount = taxAmount,
+                    price = price,
+                    extPrice = extPrice,
+                )
+            } else item.copy(
+                extPrice = 100f,
+
+                )
         }
     }
 }
